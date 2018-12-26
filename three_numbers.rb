96711b5a54f83c45c1a83048e77fc1c3ebbb46c6
antoinequellier <antoinecquellier@gmail.com>
2018-12-26 10:04:26 +0000

    three number algorithm

--- three_numbers.rb
+++ three_numbers.rb
@@ -1,8 +1,27 @@
+# def ThreeNumbers(str)
+#   # code goes here
+#   return false unless str.split(' ').each { |phrase| phrase.scan(/\d/).length == 3 }
+#   results = []
+#   str.split(' ').each do |phrase|
+#       first, second, third = phrase.scan(/\d/)[0], phrase.scan(/\d/)[1], phrase.scan(/\d/)[2]
+#       return false unless
+#       (phrase.index(third) - phrase.index(second) > 1 ||
+#       phrase.index(second) - phrase.index(first) > 1) &&
+#       first != second &&
+#       second != third
+#   end
+#   true
+# end
+
+# p ThreeNumbers("wd45gfd6 wdfgds23o9 fk8fds9fds1")
+
+
 def ThreeNumbers(str)
   # code goes here
-  return false unless str.split(' ').each { |phrase| phrase.scan(/\d/).length == 3 }
+  phrase_array = str.split(' ')
+  return false unless phrase_array.each { |phrase| phrase.scan(/\d/).length == 3 }
   results = []
-  str.split(' ').each do |phrase|
+  phrase_array.each do |phrase|
       first, second, third = phrase.scan(/\d/)[0], phrase.scan(/\d/)[1], phrase.scan(/\d/)[2]
       return false unless
       (phrase.index(third) - phrase.index(second) > 1 ||
@@ -13,4 +32,4 @@ def ThreeNumbers(str)
   true
 end
 
-p ThreeNumbers("wd45gfd6 wdfgds23o9 fk8fds9fds1")
+p ThreeNumbers("wd45gfd6 wdfgds23o9 fk8fds9fds1 fdsfdsf")
