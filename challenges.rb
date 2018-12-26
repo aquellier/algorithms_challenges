5ab5d5c0afbd63abdad5fac14fbf38b58b4c5817
antoinequellier <antoinecquellier@gmail.com>
2018-12-26 12:01:50 +0000

    new file for challenges in the plane

---
+++ challenges.rb
@@ -0,0 +1,59 @@
+# Have the function PrimeChecker(num) take num and return 1 if any arrangement of num comes out to be a prime number, otherwise return 0. For example: if num is 910, the output should be 1 because 910 can be arranged into 109 or 019, both of which are primes.
+Sample Test Cases
+Input:98
+
+Output:1
+
+
+Input:598
+
+Output:1
+
+# Have the function Consecutive(arr) take the array of integers stored in arr and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number. For example: If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Negative numbers may be entered as parameters and no array will have less than 2 elements.
+Sample Test Cases
+Input:5,10,15
+
+Output:8
+
+
+Input:-2,10,4
+
+Output:10
+
+
+# Have the function SwapII(str) take the str parameter and swap the case of each character. Then, if a letter is between two numbers (without separation), switch the places of the two numbers. For example: if str is "6Hello4 -8World, 7 yes3" the output should be 4hELLO6 -8wORLD, 7 YES3.
+Sample Test Cases
+Input:"Hello -5LOL6"
+
+Output:"hELLO -6lol5"
+
+
+Input:"2S 6 du5d4e"
+
+Output:"2s 6 DU4D5E"
+
+
+# Have the function FibonacciChecker(num) return the string yes if the number given is part of the Fibonacci sequence. This sequence is defined by: Fn = Fn-1 + Fn-2, which means to find Fn you add the previous two numbers up. The first two numbers are 0 and 1, then comes 1, 2, 3, 5 etc. If num is not in the Fibonacci sequence, return the string no.
+Sample Test Cases
+Input:34
+
+Output:"yes"
+
+
+Input:54
+
+Output:"no"
+
+# Challenge
+Have the function MaxSubarray(arr) take the array of numbers stored in arr and determine the largest sum that can be formed by any contiguous subarray in the array. For example, if arr is [-2, 5, -1, 7, -3] then your program should return 11 because the sum is formed by the subarray [5, -1, 7]. Adding any element before or after this subarray would make the sum smaller.
+Sample Test Cases
+Input:1, -2, 0, 3
+
+Output:3
+
+
+Input:3, -1, -1, 4, 3, -1
+
+Output:8
+
+# Have the function StringReduction(str) take the str parameter being passed and return the smallest number you can get through the following reduction method. The method is: Only the letters a, b, and c will be given in str and you must take two different adjacent characters and replace it with the third. For example "ac" can be replaced with "b" but "aa" cannot be replaced with anything. This method is done repeatedly until the string cannot be further reduced, and the length of the resulting string is to be outputted. For example: if str is "cab", "ca" can be reduced to "b" and you get "bb" (you can also reduce it to "cc"). The reduction is done so the output should be 2. If str is "bcab", "bc" reduces to "a", so you have "aab", then "ab" reduces to "c", and the final string "ac" is reduced to "b" so the output should be 1.
