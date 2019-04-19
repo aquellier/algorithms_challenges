80cbacb234c7e7b72f31572e402584c3059e5075
antoinequellier <antoinecquellier@gmail.com>
2019-04-19 16:31:59 +0100

    add Rakefile for array multiplication

--- array_multiplication/array_multiplication.rb
+++ array_multiplication/array_multiplication.rb
@@ -1,8 +1,5 @@
-# function must return an array of products of all the other numbers in the array
 def array_multiplication(arr)
   products = []
-  arr.each {|number| products << arr.inject(:*) / number}
-  products.join("-")
+  arr.each { |number| products << arr.inject(:*) / number }
+  products.join('-')
 end
-
-p array_multiplication([1, 4, 5, 7])
--- array_multiplication/spec/array_multiplication_spec.rb
+++ array_multiplication/spec/array_multiplication_spec.rb
@@ -1,10 +1,11 @@
-require_relative 'array_multiplication'
+require_relative '../array_multiplication'
 
 describe '#array_multiplication' do
   it 'should return a string' do
     expect(array_multiplication([1, 4, 5, 7])).to be_a String
   end
-  it "should return products of all the other numbers in the array separated by '-'" do
-    expect(array_multiplication([1, 4, 5, 7])).to eq("140-35-28-20")
+  it "should return products of all the other numbers
+  in the array separated by '-'" do
+    expect(array_multiplication([1, 4, 5, 7])).to eq('140-35-28-20')
   end
 end
