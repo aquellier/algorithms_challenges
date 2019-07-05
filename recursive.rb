f92c4d268100a9a220aca85a020d6e1572470efd
antoinequellier <antoinecquellier@gmail.com>
2019-07-05 12:22:33 +0100

    recursive function

--- recursive.rb
+++ recursive.rb
@@ -1,12 +1,27 @@
 def recursive(x)
-  if x === 0
-    0
-  elsif x === 1
-    3
-  else
-    3 * recursive(x - 1) - recursive(x - 2)
-  end
+  return 0 if x === 0
+  return 3 if x === 1
+  3 * recursive(x - 1) - recursive(x - 2)
 end
 
+def non_recursive(x)
+  return 0 if x === 0
+  return 3 if x === 1
 
-p recursive(4)
+  previous_num = 0
+  current_num = 3
+  (x - 1).times do
+    new_num = 3 * current_num - previous_num
+    previous_num = current_num
+    current_num = new_num
+  end
+  current_num
+end
+
+p non_recursive(0)
+p non_recursive(1)
+p non_recursive(2)
+p non_recursive(3)
+p non_recursive(4)
+p non_recursive(5)
+p non_recursive(6)
